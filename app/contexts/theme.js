233a9ad4872f9506f01f48d53d6a6c2855dee8c3
Keith Gibson <keithgibson@Keiths-MacBook-Air.local>
2023-05-25 14:02:49 +0100

    first commit

---
+++ app/contexts/theme.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const ThemeContext = React.createContext({
+	theme: null,
+	toggleTheme: () => {},
+});
+
+export const ThemeProvider = ({ children }) => {
+	const [theme, setTheme] = React.useState('light');
+
+	const toggleTheme = () => {
+		setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+	};
+
+	const value = { theme, toggleTheme };
+
+	return (
+		<ThemeContext.Provider value={value}>{children}</ThemeContext.Provider>
+	);
+};
